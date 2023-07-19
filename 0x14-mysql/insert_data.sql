078cb89807f43b7c6e209bf49abcd44dc5c5d64a
Sampul-CodeMine <studysampul@gmail.com>
2023-07-19 20:36:30 +0100

    SQL to create table

---
+++ 0x14-mysql/insert_data.sql
@@ -0,0 +1,7 @@
+CREATE SCHEMA IF NOT EXISTS `tyrell_corp`;
+CREATE TABLE IF NOT EXISTS `nexus6`(
+   `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   `name` VARCHAR(255) NOT NULL
+);
+INSERT INTO `tyrell_corp`.`nexus6` (`name`) VALUES ('Leon');
+GRANT SELECT ON `tyrell_corp`.* TO 'holberton_user'@'localhost';
