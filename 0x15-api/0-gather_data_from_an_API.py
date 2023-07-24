a154893e935982e161ff6d6702cd8b6a91febc9d
Sampul-CodeMine <studysampul@gmail.com>
2023-07-24 18:27:48 +0100

    Solution to Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,24 +9,25 @@ if __name__ == "__main__":
     import requests as rq
     import sys
 
-    uid = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = rq.get("{}users/{}".format(url, uid))
+    uid = int(sys.argv[1])
+    if type(uid) == int:
+        url = "https://jsonplaceholder.typicode.com/"
+        user = rq.get("{}users/{}".format(url, uid))
 
-    name = user.json().get('name')
+        name = user.json().get('name')
 
-    todos = rq.get('{}users/{}/todos'.format(url, uid))
-    tasks = todos.json()
-    totalTD = []
-    completed = 0
+        todos = rq.get('{}users/{}/todos'.format(url, uid))
+        tasks = todos.json()
+        totalTD = []
+        completed = 0
 
-    for task in tasks:
-        if task.get('completed'):
-            totalTD.append(task)
-            completed += 1
+        for task in tasks:
+            if task.get('completed'):
+               totalTD.append(task)
+               completed += 1
 
-    print('Employee {} is done with tasks({}/{}):'
-          .format(name, completed, len(tasks)))
+        print('Employee {} is done with tasks({}/{}):'
+              .format(name, completed, len(tasks)))
 
-    for task in totalTD:
-        print("\t{}".format(task.get('title')))
+        for task in totalTD:
+            print("\t {}".format(task.get('title')))
