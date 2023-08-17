841e57e28996ceb2b7e322aea2714cd4f7937c4e
Sampul-CodeMine <studysampul@gmail.com>
2023-08-17 08:57:24 +0100

    Fix for Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,14 @@
+#!/usr/bin/env bash
 # manuscript that increases the amount of traffic an NGINX server can handle
+
+# Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path => '/usr/local/bin/:/bin/'
-} ->
+  path    => '/usr/local/bin/:/bin/'
+}
 
-#Restart NGINX server
-exec { 'nginx-restart':
+# Restart Nginx
+-> exec { 'nginx-restart':
   command => 'nginx restart',
-  path => '/etc/init.d/'
+  path    => '/etc/init.d/'
 }
