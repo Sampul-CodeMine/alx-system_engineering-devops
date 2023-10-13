2376fa4378346683d090de7e31cd562b956451ff
Sampul-CodeMine <studysampul@gmail.com>
2023-10-13 08:31:14 +0100

    Tested for bugs

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # A puppet manuscript to replace a line in a file on a server
-# replace line containing "phpp" with "php"
+# replace line containing "phpp" with "php" on the php script
 exec { 'replace_line':
   command => "sed -i 's/phpp/php/g' '/var/www/html/wp-settings.php'",
   path    => ['/bin','/usr/bin']
