da0a350a1f0936876ce4d0dbe2f8317668545c03
Sampul-CodeMine <studysampul@gmail.com>
2024-01-17 13:30:30 +0100

    Updates to Web Server tasks

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,7 +8,7 @@ file_line { 'aaaaa':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/@sampulcodemine/playlists/ permanent;',
+  line   => 'rewrite ^/redirect_me https://github.com/Sampul-CodeMine/ permanent;',
 }
 
 file { '/var/www/html/index.html':
